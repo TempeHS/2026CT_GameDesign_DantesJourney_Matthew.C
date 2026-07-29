b85b954ea81702bfbf4895934496b88c47fbcc3e
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-07-29 10:35:02 +1000

    WIP Background Parallax

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    Transform cam;
+    Vector3 camStartPos;
+    float distance;
+
+    GameObject[] backgrounds;
+    Material[] mat;
+    float[] backSpeed;
+
+    float farthestBack;
+
+    [Range(0.01f, 0.05f)]
+    public float parallaxSpeed;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        camStartPos = cam.position;
+
+        int backCount = transform.childCount;
+        mat = new Material[backCount];
+        backSpeed = new float[backCount];
+        backgrounds = new gameObject[backCount];
+
+        for (int i = 0; i < backCount ; i++)
+        {
+            backgrounds[i] = transform.GetChild(i).gameObject;
+            mat[i]=backgrounds[i].GetComponent<Renderer>().material;
+        }
+    }
+
+    void BackSpeedCalculate(int backCount)
+    {
+        for (int i = 0; i < backCount; i++)
+        {
+            
+        }
+    }
+}
