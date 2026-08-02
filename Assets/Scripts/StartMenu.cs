64aa8dafcc38a3809b3cf1ae291573b0465d9333
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-08-03 09:40:25 +1000

    WIP: Damage + Health system

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class StartMenu : MonoBehaviour
 {
+
+    AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    private void Start() 
+    {
+        audioManager.PlaySFX(audioManager.titleScreenMusic);
+    }
+
     public void PlayGame() 
     {
         SceneManager.LoadSceneAsync("Limbo");
