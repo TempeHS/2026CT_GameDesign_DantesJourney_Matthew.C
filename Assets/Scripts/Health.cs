abf2eb41dd6b3b139b84034b1a9ad7ac94d7e627
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-08-12 10:35:34 +1000

    WIP Moving Saw

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,17 +3,33 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
-    private float currentHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
     private void Awake() 
     {
         currentHealth = startingHealth;
-
+        anim = GetComponent<Animator>();
     }
 
     public void takeDamage (float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-        currentHealth -= _damage;
+
+        if (currentHealth > 0) 
+        {
+            anim.SetTrigger("hurt");
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("death");
+                GetComponent<PlayerMovement>().enabled = false;   
+                dead = true;
+            }
+
+        }
     }
 }
