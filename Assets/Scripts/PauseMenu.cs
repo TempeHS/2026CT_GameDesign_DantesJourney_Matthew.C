2706ce87137c344b77e5831cefe5ab5368aa6946
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-09-14 09:40:02 +1000

    UPD + Fixes

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -9,23 +9,27 @@ public class Paused : MonoBehaviour
     {
             paused.SetActive(true);
             Time.timeScale = 0;
+            AudioListener.pause = true;
     }
 
     public void Resume()
     {
             paused.SetActive(false);
             Time.timeScale = 1;
+            AudioListener.pause = false;
     }
 
     public void ReturnMainMenu()
     {
         SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1;
+        AudioListener.pause = false;
     }
 
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+        AudioListener.pause = false;
     }
 }
