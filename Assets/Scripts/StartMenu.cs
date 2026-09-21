dbda53c921fb7af67dfcf2251b240af549abf3d5
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-09-21 11:54:56 +1000

    Build and Run

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,19 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-
-    AudioManager audioManager;
-
-    private void Awake()
-    {
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-    }
-
-    private void Start() 
-    {
-        audioManager.PlaySFX(audioManager.titleScreenMusic);
-    }
-
     public void PlayGame() 
     {
         SceneManager.LoadSceneAsync("Limbo");
